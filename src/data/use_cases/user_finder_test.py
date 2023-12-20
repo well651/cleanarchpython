b2f018d63819b6fc97b67274d314c069ca7d56a5
wellington <wellington652@hotmail.com>
2023-12-20 20:31:19 -0300

    feat: adding UserRepositorySPy and userfind methods

--- src/data/use_cases/user_finder_test.py
+++ src/data/use_cases/user_finder_test.py
@@ -1,7 +1,42 @@
-from src.infra.db.repositories.users_repository import UsersRepository
+#pylint: disable = use-implicit-booleaness-not-comparison
+from src.infra.db.tests.users_repository import UsersRepositorySpy
 from .user_finder import UserFinder
 
 def test_find():
-    repo = UsersRepository()
+    first_name = 'name'
+    repo = UsersRepositorySpy()
     user_finder = UserFinder(repo)
 
+    response = user_finder.find(first_name)
+
+    assert repo.select_user_attributes['first_name'] == first_name
+
+    assert response["type"] == "Users"
+    assert response["count"] == len(response["attributes"])
+    assert response["attributes"] != []
+
+def test_find_error_in_valid_name():
+    first_name = 'name123'
+    repo = UsersRepositorySpy()
+    user_finder = UserFinder(repo)
+
+    try:
+        user_finder.find(first_name)
+        assert False
+    except Exception as exception:
+        assert str(exception) == "Invalid format for first name"
+
+def test_find_error_user_not_found():
+    class UserRepositoryError(UsersRepositorySpy):
+        def select_user(self, first_name: str):
+            return []
+
+    first_name = 'name'
+    repo = UserRepositoryError()
+    user_finder = UserFinder(repo)
+
+    try:
+        user_finder.find(first_name)
+        assert False
+    except Exception as exception:
+        assert str(exception) == "User not found"
